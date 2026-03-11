13ca58ab03c3e6bbf6bcab7cde7abefbc8035952
josefbar <josefbar@student.21-school.ru>
2026-03-11 17:24:07 +0300

    fixprice

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -1,5 +1,10 @@
-#include <stdarg.h>
+#include <limits.h>
 #include <math.h>
+#include <stdarg.h>
+#include <string.h>
+#include <wchar.h>
+
+#include "s21_sprintf.h"
 #include "s21_string.h"
 
 static void parse_format(const char *fmt, int *i, s21_fmt_t *f) {
@@ -8,7 +13,6 @@ static void parse_format(const char *fmt, int *i, s21_fmt_t *f) {
   f->precision = -1;
   f->length = 0;
 
-  // flags
   while (fmt[*i] == '-' || fmt[*i] == '+' || fmt[*i] == ' ' || fmt[*i] == '0') {
     if (fmt[*i] == '-') f->minus = 1;
     if (fmt[*i] == '+') f->plus = 1;
@@ -17,13 +21,11 @@ static void parse_format(const char *fmt, int *i, s21_fmt_t *f) {
     (*i)++;
   }
 
-  // width
   while (fmt[*i] >= '0' && fmt[*i] <= '9') {
     f->width = f->width * 10 + (fmt[*i] - '0');
     (*i)++;
   }
 
-  // precision
   if (fmt[*i] == '.') {
     f->precision = 0;
     (*i)++;
@@ -33,25 +35,44 @@ static void parse_format(const char *fmt, int *i, s21_fmt_t *f) {
     }
   }
 
-  // length
   if (fmt[*i] == 'h' || fmt[*i] == 'l') {
     f->length = fmt[*i];
     (*i)++;
   }
 
-  // spec
   f->spec = fmt[*i];
   (*i)++;
 }
 
 static void print_spaces(char *str, int *j, int count) {
-  for (int i = 0; i < count; i++)
-    str[(*j)++] = ' ';
+  for (int i = 0; i < count; i++) str[(*j)++] = ' ';
 }
 
 static void print_zeros(char *str, int *j, int count) {
-  for (int i = 0; i < count; i++)
-    str[(*j)++] = '0';
+  for (int i = 0; i < count; i++) str[(*j)++] = '0';
+}
+
+static void emit_padded(char *str, int *j, s21_fmt_t f, char sign,
+                        const char *body, int body_len) {
+  int total_len = body_len + (sign ? 1 : 0);
+  int pad = f.width > total_len ? f.width - total_len : 0;
+  int zero_pad = f.zero && !f.minus;
+
+  if (!f.minus) {
+    if (zero_pad) {
+      if (sign) {
+        str[(*j)++] = sign;
+        sign = 0;
+      }
+      print_zeros(str, j, pad);
+    } else {
+      print_spaces(str, j, pad);
+    }
+  }
+
+  if (sign) str[(*j)++] = sign;
+  for (int k = 0; k < body_len; k++) str[(*j)++] = body[k];
+  if (f.minus) print_spaces(str, j, pad);
 }
 
 static int to_digits_ull(char *buf, unsigned long long x) {
@@ -71,29 +92,51 @@ static void print_char(char *str, int *j, s21_fmt_t f, char c) {
   if (f.minus) print_spaces(str, j, pad);
 }
 
+static int wchar_to_mb(char *out, size_t cap, wchar_t wc) {
+  mbstate_t st;
+  memset(&st, 0, sizeof(st));
+  size_t n = wcrtomb(out, wc, &st);
+  int res = (n == (size_t)-1 || n >= cap) ? -1 : (int)n;
+  return res;
+}
+
+static void print_wchar(char *str, int *j, s21_fmt_t f, wint_t wc) {
+  char mb[MB_LEN_MAX];
+  int len = wchar_to_mb(mb, sizeof(mb), (wchar_t)wc);
+  int pad = (len > 0 && f.width > len) ? f.width - len : 0;
+
+  if (len > 0) {
+    if (!f.minus) print_spaces(str, j, pad);
+    for (int k = 0; k < len; k++) str[(*j)++] = mb[k];
+    if (f.minus) print_spaces(str, j, pad);
+  }
+}
+
 static void print_int(char *str, int *j, s21_fmt_t f, long long x) {
   char buf[32];
   int neg = x < 0;
-  unsigned long long abs_x = neg ? (unsigned long long)(-(x + 1)) + 1ULL : (unsigned long long)x;
+  unsigned long long abs_x =
+      neg ? (unsigned long long)(-(x + 1)) + 1ULL : (unsigned long long)x;
 
-  int digits_len = to_digits_ull(buf, abs_x);  // без знака
+  int digits_len = to_digits_ull(buf, abs_x);
 
-  if (digits_len == 1 && buf[0] == '0' && f.precision == 0)
-    digits_len = 0;
+  if (digits_len == 1 && buf[0] == '0' && f.precision == 0) digits_len = 0;
 
   int zeros = 0;
-  if (f.precision > digits_len)
-    zeros = f.precision - digits_len;
-  
+  if (f.precision > digits_len) zeros = f.precision - digits_len;
+
   char sign = 0;
-  if (neg) sign = '-';
-  else if (f.plus) sign = '+';
-  else if (f.space) sign = ' ';
+  if (neg)
+    sign = '-';
+  else if (f.plus)
+    sign = '+';
+  else if (f.space)
+    sign = ' ';
 
   int total_len = digits_len + zeros + (sign ? 1 : 0);
   int pad = f.width > total_len ? f.width - total_len : 0;
   int zero_pad = f.zero && !f.minus && f.precision == -1;
-  
+
   if (!f.minus) {
     if (zero_pad) {
       if (sign) {
@@ -115,97 +158,119 @@ static void print_int(char *str, int *j, s21_fmt_t f, long long x) {
   if (f.minus) print_spaces(str, j, pad);
 }
 
-static void print_float(char *str, int *j, s21_fmt_t f, double x) {
-  char int_buf[32];
-  char frac_buf[32];
+static void format_float_parts(double x, int precision, s21_fmt_t f,
+                               char *int_buf, int *int_len, char *frac_buf,
+                               int *frac_len, char *sign) {
+  if (precision == -1) precision = 6;
+  if (precision > 18) precision = 18;
 
-  if (f.precision == -1) f.precision = 6;
-  if (f.precision > 18) f.precision = 18;
-
-  char sign = 0;
-  if (x < 0) {
-    sign = '-';
+  *sign = 0;
+  if (signbit(x)) {
+    *sign = '-';
     x = -x;
   } else if (f.plus) {
-    sign = '+';
+    *sign = '+';
   } else if (f.space) {
-    sign = ' ';
+    *sign = ' ';
   }
 
   unsigned long long scale = 1;
-  for (int i = 0; i < f.precision; i++) scale *= 10ULL;
-  unsigned long long rounded = (unsigned long long)llrintl((long double)x * (long double)scale);
+  for (int i = 0; i < precision; i++) scale *= 10ULL;
+  unsigned long long rounded =
+      (unsigned long long)llrintl((long double)x * (long double)scale);
   unsigned long long int_part = rounded / scale;
   unsigned long long frac_int = rounded % scale;
 
-  int int_len = to_digits_ull(int_buf, int_part);
-  int frac_len = f.precision;
+  *int_len = to_digits_ull(int_buf, int_part);
+  *frac_len = precision;
 
-  for (int k = frac_len - 1; k >= 0; k--) {
+  for (int k = precision - 1; k >= 0; k--) {
     frac_buf[k] = (char)('0' + (frac_int % 10));
     frac_int /= 10ULL;
   }
+}
 
-  int number_len = int_len + (frac_len > 0 ? 1 + frac_len : 0);
-  int total_len = number_len + (sign ? 1 : 0);
-  int pad = f.width > total_len ? f.width - total_len : 0;
-  int zero_pad = f.zero && !f.minus;
-
-  if (!f.minus) {
-    if (zero_pad) {
-      if (sign) {
-        str[(*j)++] = sign;
-        sign = 0;
-      }
-      print_zeros(str, j, pad);
-    } else {
-      print_spaces(str, j, pad);
+static void print_float(char *str, int *j, s21_fmt_t f, double x) {
+  if (isnan(x) || isinf(x)) {
+    char sign = signbit(x) ? '-' : (f.plus ? '+' : (f.space ? ' ' : 0));
+    const char *word = isnan(x) ? "nan" : "inf";
+    emit_padded(str, j, f, sign, word, 3);
+  } else {
+    char int_buf[32], frac_buf[32], out[64], sign;
+    int int_len, frac_len, pos = 0;
+    format_float_parts(x, f.precision, f, int_buf, &int_len, frac_buf, &frac_len,
+                       &sign);
+    while (int_len--) out[pos++] = int_buf[int_len];
+    if (frac_len > 0) {
+      out[pos++] = '.';
+      memcpy(out + pos, frac_buf, (size_t)frac_len);
+      pos += frac_len;
     }
+    emit_padded(str, j, f, sign, out, pos);
   }
-
-  if (sign) str[(*j)++] = sign;
-
-  while (int_len--) str[(*j)++] = int_buf[int_len];
-
-  if (frac_len > 0) {
-    str[(*j)++] = '.';
-    for (int k = 0; k < frac_len; k++) str[(*j)++] = frac_buf[k];
-  }
-
-  if (f.minus) print_spaces(str, j, pad);
 }
 
 static void print_string(char *str, int *j, s21_fmt_t f, char *s) {
   int len = 0;
-  while (s[len] && (f.precision == -1 || len < f.precision))
-    len++;
+  while (s[len] && (f.precision == -1 || len < f.precision)) len++;
 
   int pad = f.width > len ? f.width - len : 0;
 
   if (!f.minus) print_spaces(str, j, pad);
-  for (int i = 0; i < len; i++)
-    str[(*j)++] = s[i];
+  for (int i = 0; i < len; i++) str[(*j)++] = s[i];
   if (f.minus) print_spaces(str, j, pad);
+}
+
+static void print_wstring(char *str, int *j, s21_fmt_t f, const wchar_t *ws) {
+  if (!ws) ws = L"(null)";
+
+  char tmp[4096];
+  int pos = 0;
+  int stop = 0;
+  int emit = 1;
+
+  for (int i = 0; ws[i] != L'\0' && !stop; i++) {
+    char mb[MB_LEN_MAX];
+    int len = wchar_to_mb(mb, sizeof(mb), ws[i]);
+    if (len <= 0) {
+      pos = 0;
+      stop = 1;
+      emit = 0;
+    }
+    if (!stop && f.precision != -1 && pos + len > f.precision) stop = 1;
+    if (!stop && pos + len >= (int)sizeof(tmp) - 1) stop = 1;
+    if (!stop) {
+      for (int k = 0; k < len; k++) tmp[pos++] = mb[k];
+    }
+  }
+
+  tmp[pos] = '\0';
+
+  if (emit) {
+    s21_fmt_t f2 = f;
+    f2.precision = -1;
+    print_string(str, j, f2, tmp);
+  }
 }
 
 static void print_uint(char *str, int *j, s21_fmt_t f, unsigned long long x) {
   char buf[32];
   int digits_len = to_digits_ull(buf, x);
 
-  if (digits_len == 1 && buf[0] == '0' && f.precision == 0)
-    digits_len = 0;
+  if (digits_len == 1 && buf[0] == '0' && f.precision == 0) digits_len = 0;
 
   int zeros = 0;
-  if (f.precision > digits_len)
-    zeros = f.precision - digits_len;
+  if (f.precision > digits_len) zeros = f.precision - digits_len;
 
   int total_len = digits_len + zeros;
   int pad = f.width > total_len ? f.width - total_len : 0;
   int zero_pad = f.zero && !f.minus && f.precision == -1;
 
   if (!f.minus) {
-    if (zero_pad) print_zeros(str, j, pad);
-    else print_spaces(str, j, pad);
+    if (zero_pad)
+      print_zeros(str, j, pad);
+    else
+      print_spaces(str, j, pad);
   }
 
   print_zeros(str, j, zeros);
@@ -215,53 +280,68 @@ static void print_uint(char *str, int *j, s21_fmt_t f, unsigned long long x) {
   if (f.minus) print_spaces(str, j, pad);
 }
 
-int s21_sprintf(char *str, const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
+static long long get_signed_arg(va_list *ap, s21_fmt_t f) {
+  long long res;
+  if (f.length == 'h')
+    res = (short)va_arg(*ap, int);
+  else if (f.length == 'l')
+    res = va_arg(*ap, long);
+  else
+    res = va_arg(*ap, int);
+  return res;
+}
 
-    int i = 0, j = 0;
+static unsigned long long get_unsigned_arg(va_list *ap, s21_fmt_t f) {
+  unsigned long long res;
+  if (f.length == 'h')
+    res = (unsigned short)va_arg(*ap, unsigned int);
+  else if (f.length == 'l')
+    res = va_arg(*ap, unsigned long);
+  else
+    res = va_arg(*ap, unsigned int);
+  return res;
+}
 
-    while (fmt[i]) {
-        if (fmt[i] != '%') {
-            str[j++] = fmt[i++];
-            continue;
-        }
-        i++;
-        s21_fmt_t f;
-        parse_format(fmt, &i, &f);
-
-        if (f.spec == 'c') {
-            print_char(str, &j, f, va_arg(ap, int));
-        }
-        else if (f.spec == 's') {
-            char *s = va_arg(ap, char *);
-            if (!s) s = "(null)";
-            print_string(str, &j, f, s);
-        }
-        else if (f.spec == 'd') {
-            long long val;
-            if (f.length == 'h') val = (short)va_arg(ap, int);
-            else if (f.length == 'l') val = va_arg(ap, long);
-            else val = va_arg(ap, int);
-            print_int(str, &j, f, val);
-        }
-        else if (f.spec == 'u') {
-            unsigned long long val;
-            if (f.length == 'h') val = (unsigned short)va_arg(ap, unsigned int);
-            else if (f.length == 'l') val = va_arg(ap, unsigned long);
-            else val = va_arg(ap, unsigned int);
-            print_uint(str, &j, f, val);
-        }
-        else if (f.spec == 'f') {
-            double val = va_arg(ap, double);
-            print_float(str, &j, f, val);
-        }
-        else if (f.spec == '%') {
-            str[j++] = '%';
-        }
+static void handle_spec(char *str, int *j, s21_fmt_t f, va_list *ap) {
+  if (f.spec == 'c') {
+    if (f.length == 'l')
+      print_wchar(str, j, f, va_arg(*ap, wint_t));
+    else
+      print_char(str, j, f, va_arg(*ap, int));
+  } else if (f.spec == 's') {
+    if (f.length == 'l') {
+      print_wstring(str, j, f, va_arg(*ap, const wchar_t *));
+    } else {
+      char *s = va_arg(*ap, char *);
+      if (!s) s = "(null)";
+      print_string(str, j, f, s);
     }
+  } else if (f.spec == 'd') {
+    print_int(str, j, f, get_signed_arg(ap, f));
+  } else if (f.spec == 'u') {
+    print_uint(str, j, f, get_unsigned_arg(ap, f));
+  } else if (f.spec == 'f') {
+    print_float(str, j, f, va_arg(*ap, double));
+  } else if (f.spec == '%') {
+    str[(*j)++] = '%';
+  }
+}
 
-    str[j] = '\0';
-    va_end(ap);
-    return j;
+int s21_sprintf(char *str, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int i = 0, j = 0;
+  while (fmt[i]) {
+    if (fmt[i] != '%') {
+      str[j++] = fmt[i++];
+      continue;
+    }
+    i++;
+    s21_fmt_t f;
+    parse_format(fmt, &i, &f);
+    handle_spec(str, &j, f, &ap);
+  }
+  str[j] = '\0';
+  va_end(ap);
+  return j;
 }
